28c859b9f7446531292659355ff17749d8def670
Dániel Buga <bugadani@gmail.com>
2025-04-30 22:28:12 +0200

    Implement voltage control

--- src/main.rs
+++ src/main.rs
@@ -7,26 +7,28 @@ use dap_rs::jtag::TapConfig;
 use dap_rs::swo::Swo;
 use defmt::{todo, unwrap, warn};
 use embassy_executor::Spawner;
-use embassy_futures::join::join;
+use embassy_futures::join::join3;
+use embassy_rp::adc::{self, Adc};
 use embassy_rp::flash::Flash;
-use embassy_rp::gpio::{Flex, Level, Output, Pin};
-use embassy_rp::peripherals::USB;
-use embassy_rp::usb::{Driver as UsbDriver, InterruptHandler};
+use embassy_rp::gpio::{Flex, Input, Level, Output, Pin, Pull};
+use embassy_rp::peripherals::{PIN_0, PIN_5, PWM_SLICE0, PWM_SLICE2, USB};
+use embassy_rp::pwm::{self, Pwm, SetDutyCycle};
+use embassy_rp::usb::{self, Driver as UsbDriver};
 use embassy_rp::{Peri, bind_interrupts};
+use embassy_time::{Duration, Ticker};
 use embassy_usb::class::cdc_acm::CdcAcmClass;
 use embassy_usb::class::cdc_acm::State;
 use embassy_usb::class::cmsis_dap_v2::{CmsisDapV2Class, State as CmsisDapV2State};
 use embassy_usb::msos::windows_version;
-use embassy_usb::{Builder, Config};
+use embassy_usb::{self, Builder};
 use heapless::String;
 use static_cell::{ConstStaticCell, StaticCell};
 
 use {defmt_rtt as _, panic_probe as _};
 
 bind_interrupts!(struct Irqs {
-    USBCTRL_IRQ => InterruptHandler<USB>;
+    USBCTRL_IRQ => usb::InterruptHandler<USB>;
 });
-
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
@@ -41,6 +43,28 @@ async fn main(_spawner: Spawner) {
     let dir_swclk = p.PIN_19;
     let t_jtdo = p.PIN_16;
     //let t_swo // Not supported yet
+
+    let mut target_vcc = TargetVccReader {
+        pin: adc::Channel::new_pin(p.PIN_26, Pull::None),
+        adc: Adc::new_blocking(p.ADC, adc::Config::default()),
+    };
+
+    let mut target_physically_connected = TargetPhysicallyConnected {
+        pin: Input::new(p.PIN_8, Pull::Up),
+    };
+
+    let mut translator_power = TranslatorPower::new(p.PIN_5, p.PWM_SLICE2);
+    translator_power.set_translator_vcc(1800);
+
+    let mut target_power = TargetPower::new(
+        Output::new(p.PIN_3, Level::Low),
+        // Always enable the protected 5v (existed until rev E of hardware)
+        Output::new(p.PIN_7, Level::High),
+        Output::new(p.PIN_6, Level::Low),
+        p.PIN_0,
+        p.PWM_SLICE0,
+    );
+    target_power.set_vtgt(1800);
 
     // 2. Max JTAG scan chain
     const MAX_SCAN_CHAIN_LENGTH: usize = 8;
@@ -75,7 +99,7 @@ async fn main(_spawner: Spawner) {
     }
 
     // Create embassy-usb Config
-    let mut config = Config::new(0xc0de, 0xcafe);
+    let mut config = embassy_usb::Config::new(0xc0de, 0xcafe);
     config.manufacturer = Some(MANUFACTURER);
     config.product = Some(PRODUCT);
     config.serial_number = Some(serial);
@@ -124,18 +148,18 @@ async fn main(_spawner: Spawner) {
     static SCAN_CHAIN: ConstStaticCell<[TapConfig; MAX_SCAN_CHAIN_LENGTH]> =
         ConstStaticCell::new([TapConfig::INIT; MAX_SCAN_CHAIN_LENGTH]);
     let deps = BitbangAdapter::new(
-        IoPin::new(t_nrst, None),
-        IoPin::new(t_jtdi, None),
+        IoPin::new(t_nrst, None::<Peri<'static, PIN_0>>),
+        IoPin::new(t_jtdi, None::<Peri<'static, PIN_0>>),
         IoPin::new(t_jtms_swdio, Some(dir_swdio)),
         IoPin::new(t_jtck_swclk, Some(dir_swclk)),
-        IoPin::new(t_jtdo, None),
+        IoPin::new(t_jtdo, None::<Peri<'static, PIN_0>>),
         BitDelay,
         SCAN_CHAIN.take(),
     );
     let mut dap = Dap::new(
         deps,
         Leds {
-            red: Output::new(p.PIN_28, Level::High),
+            _red: Output::new(p.PIN_28, Level::High),
             green: Output::new(p.PIN_27, Level::High),
             blue: Output::new(p.PIN_29, Level::High),
         },
@@ -165,9 +189,32 @@ async fn main(_spawner: Spawner) {
         }
     };
 
+    let voltage_control_fut = async {
+        let mut ticker = Ticker::every(Duration::from_millis(100));
+        loop {
+            // Set the voltage translators to track Target's VCC
+            let target_vcc_mv = target_vcc.read_voltage_mv();
+
+            if target_vcc_mv > 1500 {
+                translator_power.set_translator_vcc(target_vcc_mv);
+            } else {
+                if target_physically_connected.target_detected() {
+                    // If there is no VCC detected use 3.3v.
+                    translator_power.set_translator_vcc(3300);
+                } else {
+                    translator_power.set_translator_vcc(0);
+                }
+            }
+
+            defmt::trace!("Tracking Target VCC at {} mV", target_vcc_mv);
+
+            ticker.next().await;
+        }
+    };
+
     // Run everything concurrently.
     // If we had made everything `'static` above instead, we could do this using separate tasks instead.
-    join(usb_fut, dap_fut).await;
+    join3(usb_fut, dap_fut, voltage_control_fut).await;
 }
 
 struct BitDelay;
@@ -233,7 +280,7 @@ impl InputOutputPin for IoPin<'_> {
 }
 
 struct Leds<'a> {
-    red: Output<'a>,
+    _red: Output<'a>,
     green: Output<'a>,
     blue: Output<'a>,
 }
@@ -292,5 +339,132 @@ impl Swo for NoSwo {
 
     fn status(&mut self) -> dap_rs::swo::SwoStatus {
         todo!()
+    }
+}
+
+pub struct TargetPhysicallyConnected {
+    pin: Input<'static>,
+}
+
+impl TargetPhysicallyConnected {
+    /// This checks for the target being connected via the GND detect pin.
+    pub fn target_detected(&mut self) -> bool {
+        self.pin.is_low()
+    }
+}
+
+pub struct TargetVccReader {
+    pub pin: adc::Channel<'static>,
+    pub adc: Adc<'static, adc::Blocking>,
+}
+
+impl TargetVccReader {
+    pub fn read_voltage_mv(&mut self) -> u32 {
+        let Self { pin, adc } = self;
+        let val: u16 = adc.blocking_read(pin).unwrap();
+
+        (2 * 3300 * val as u32) / 4095
+    }
+}
+
+pub struct TranslatorPower {
+    vtranslator_pwm: Pwm<'static>,
+}
+
+impl TranslatorPower {
+    pub fn new(
+        vtranslator_pin: Peri<'static, PIN_5>,
+        vtranslator_pwm: Peri<'static, PWM_SLICE2>,
+    ) -> Self {
+        // Output channel B on PWM2 to GPIO 5
+        let mut vtranslator_pwm = Pwm::new_output_b(vtranslator_pwm, vtranslator_pin, {
+            let mut c = pwm::Config::default();
+            c.top = 4095;
+            c.enable = true;
+            c.phase_correct = false;
+            c
+        });
+
+        vtranslator_pwm.set_duty_cycle(1023).unwrap();
+
+        Self { vtranslator_pwm }
+    }
+
+    pub fn set_translator_vcc(&mut self, mv: u32) {
+        // ans = 0.0900 * 4095
+        let v33 = 369; // duty cycle that give 3.3v
+
+        // ans = 0.7617 * 4095
+        let v18 = 3119; // duty cycle that gives 1.8v
+
+        let vomin = 1800; // The actual voltage when PWM is set to v18 duty cycle
+        let vomax = 3300; // The actual voltage when PWM is set to v33 duty cycle
+
+        let mv = mv.min(vomax).max(vomin);
+
+        let limit_diff = v18 - v33;
+        let mv_diff = mv - vomin;
+
+        let cnt = ((vomax - vomin - mv_diff) * limit_diff) / (vomax - vomin) + v33;
+
+        self.vtranslator_pwm.set_duty_cycle(cnt as u16).unwrap();
+    }
+}
+
+pub struct TargetPower {
+    enable_5v_key: Output<'static>,
+    enable_vtgt: Output<'static>,
+    vtgt_pwm: Pwm<'static>,
+}
+
+impl TargetPower {
+    pub fn enable_vtgt(&mut self) {
+        self.enable_vtgt.set_high();
+    }
+
+    pub fn enable_5v_key(&mut self) {
+        self.enable_5v_key.set_high();
+    }
+
+    pub fn new(
+        enable_5v_key: Output<'static>,
+        _enable_5v: Output<'static>,
+        enable_vtgt: Output<'static>,
+        vtgt_pin: Peri<'static, PIN_0>,
+        vtgt_pwm: Peri<'static, PWM_SLICE0>,
+    ) -> Self {
+        // Output channel A on PWM0 to GPIO 0
+        let mut vtgt_pwm = Pwm::new_output_a(vtgt_pwm, vtgt_pin, {
+            let mut c = pwm::Config::default();
+            c.top = 4095;
+            c.enable = true;
+            c.phase_correct = false;
+            c
+        });
+
+        vtgt_pwm.set_duty_cycle(1023).unwrap();
+
+        Self {
+            enable_5v_key,
+            enable_vtgt,
+            vtgt_pwm,
+        }
+    }
+
+    pub fn set_vtgt(&mut self, mv: u32) {
+        let v33 = 369; // duty cycle that gives 3.3v
+        let v18 = 3119; // duty cycle that gives 1.8v
+
+        let vomin = 1800; // The actual voltage when PWM is set to v18 duty cycle
+        let vomax = 3300; // The actual voltage when PWM is set to v33 duty cycle
+
+        let mv = mv.min(vomax).max(vomin);
+
+        let limit_diff = v18 - v33;
+        let mv_diff = mv - vomin;
+
+        let cnt = ((vomax - vomin - mv_diff) * limit_diff) / (vomax - vomin) + v33;
+
+        self.vtgt_pwm.set_duty_cycle(cnt as u16).unwrap();
     }
 }
